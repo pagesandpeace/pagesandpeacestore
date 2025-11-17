ca396be76cb5387ef432a4f4a61959622ea9972f
pagesandpeace <admin@pagesandpeace.co.uk>
2025-11-17 22:14:44 +0100

    dynamic derver fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -20,7 +20,7 @@ export default async function AdminHomePage() {
 
   return (
     <div className="space-y-6">
-      <h1 className="text-3xl font-bold">Welcom the Admin Dashboard</h1>
+      <h1 className="text-3xl font-bold">Welcome to the Admin Dashboard</h1>
 
       <p>Select a section from the sidebar.</p>
 
